b24eb0d18d9c8b3177691a7c804c24fbcb12eeea
unknown <santhoshkoppisetti2002@gmail.com>
2026-06-13 23:18:51 +0530

    Initial commit

--- tests/test1.spec.ts
+++ tests/test1.spec.ts
@@ -1,9 +1,33 @@
 import { test, expect } from '@playwright/test'
+import data from '../test-data/data.json'
+
 
 test('test1', async ({ page }) => {
     
     await page.goto('https://demoqa.com/')
-    await page.getByRole('heading', { name: 'Book Store Application', level: 5 })
+    await page.getByRole('heading', { name: 'Book Store Application', level: 5 }).click()
+    await page.getByRole('button', { name: 'Login' }).click()
+    await page.getByRole('button', { name: 'New User' }).click()
+    await page.getByPlaceholder('First Name').fill(data.user1['First Name'])
+    await page.getByPlaceholder('Last Name').fill(data.user1['Last Name'])
+    await page.getByPlaceholder('UserName').fill(data.user1['UserName'])
+    await page.getByPlaceholder('Password').fill(data.user1['Password'])
+    await page.getByRole('button', { name: 'Register' }).click()
+    
+    await page.waitForTimeout(30000)
+
+
+
+
+
+
+
+        
+
+    
+
+
+
  
 
 })
